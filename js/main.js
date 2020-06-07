75bceefd1399e114dc9b9a4192b7f06698c8319d
Elena Sechenykh <sechenyh@inbox.ru>
2020-06-07 11:37:23 +0300

    Исправляет названия функций и переменных, меняет функцию поиска массива случайной длины.

--- js/main.js
+++ js/main.js
@@ -1,35 +1,23 @@
 'use strict';
 
 var OBJECTS_COUNT = 8;
+var TITLE = 'Заголовок';
+var PRICE = 1000;
 var TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var ROOMS = {min: 1, max: 4};
+var GUESTS = {min: 1, max: 6};
 var CHECKINS = ['12:00', '13:00', '14:00'];
 var CHECKOUTS = ['12:00', '13:00', '14:00'];
 var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var DESCRIPTION = 'Описание';
+var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var LOCATION_X_MIN = 0;
 var LOCATION_X_MAX = 1200;
 var LOCATION_Y_MIN = 130;
 var LOCATION_Y_MAX = 630;
 var PIN_WIDTH = 50;
-var pinX = PIN_WIDTH / 2;
-var PIN_Y = 70;
-
-var getAvatarsList = function (number) {
-  var avatarsList = [];
-
-  for (var i = 1; i <= number; i++) {
-    avatarsList.push('img/avatars/user0' + i + '.png');
-  }
-  return avatarsList;
-};
-
-var avatars = getAvatarsList(OBJECTS_COUNT);
-
-var title = 'Заголовок';
-var price = 1000;
-var rooms = [1, 2, 3, 4];
-var guests = [1, 2, 3, 4, 5, 6];
-var description = 'Описание';
-var photos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var PIN_OFFSET_X = PIN_WIDTH / 2;
+var PIN_OFFSET_Y = 70;
 
 var getRandomNumber = function (min, max) {
   min = Math.ceil(min);
@@ -37,68 +25,72 @@ var getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-var getRandomValue = function (values) {
+var getRandomElementOfArray = function (values) {
   return values[Math.floor(Math.random() * values.length)];
 };
 
-var getRandomArray = function (array, min) {
-  var newArray = array.slice();
-  var numberDel = getRandomNumber(min, array.length);
-
-  for (var i = 0; i < numberDel; i++) {
-    newArray.splice(getRandomNumber(0, newArray.length - 1), 1);
+var shuffleArray = function (array) {
+  var shuffledArray = array.slice();
+  for (var i = shuffledArray.length - 1; i > 0; i--) {
+    var randomElement = Math.floor(Math.random() * (i + 1));
+    var buffer = shuffledArray[randomElement];
+    shuffledArray[randomElement] = shuffledArray[i];
+    shuffledArray[i] = buffer;
   }
-
-  return newArray;
+  return shuffledArray;
 };
 
-var makeRandomObject = function (index) {
-  var avatarAddresses = avatars[index];
+var getRandomLengthArray = function (array, min) {
+  var newArray = shuffleArray(array);
+  var randomLength = getRandomNumber(min, newArray.length - 1);
+
+  var randomLengthArray = newArray.slice(0, randomLength);
+
+  return randomLengthArray;
+};
+
+var makeRandomAd = function (index) {
+  var avatarAddress = 'img/avatars/user0' + (index + 1) + '.png';
   var locationX = getRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX);
   var locationY = getRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX);
   var location = locationX + ', ' + locationY;
-  var typeObject = getRandomValue(TYPES);
-  var roomsObject = getRandomValue(rooms);
-  var guestsObject = getRandomValue(guests);
-  var checkinObject = getRandomValue(CHECKINS);
-  var checkoutObject = getRandomValue(CHECKOUTS);
-  var featuresObjects = getRandomArray(FEATURES, 0);
-  var photoObjects = getRandomArray(photos, 0);
 
-  return {
+  var ad = {
     author: {
-      avatar: avatarAddresses
+      avatar: avatarAddress
     },
     offer: {
-      title: title,
+      title: TITLE,
       address: location,
-      price: price,
-      type: typeObject,
-      rooms: roomsObject,
-      guests: guestsObject,
-      checkin: checkinObject,
-      checkout: checkoutObject,
-      features: featuresObjects,
-      description: description,
-      photo: photoObjects
+      price: PRICE,
+      type: getRandomElementOfArray(TYPES),
+      rooms: getRandomNumber(ROOMS.min, ROOMS.max),
+      guests: getRandomNumber(GUESTS.min, GUESTS.max),
+      checkin: getRandomElementOfArray(CHECKINS),
+      checkout: getRandomElementOfArray(CHECKOUTS),
+      features: getRandomLengthArray(FEATURES, 0),
+      description: DESCRIPTION,
+      photo: getRandomLengthArray(PHOTOS, 0)
     },
     location: {
       x: locationX,
       y: locationY,
     }
   };
+
+  return ad;
 };
 
-var getObjectList = function (number) {
-  var objectList = [];
+var getAdsList = function (number) {
+  var adsList = [];
 
   for (var i = 0; i < number; i++) {
-    objectList.push(makeRandomObject(i));
+    adsList.push(makeRandomAd(i));
   }
-  return objectList;
+  return adsList;
 };
 
-var objects = getObjectList(OBJECTS_COUNT);
+var ads = getAdsList(OBJECTS_COUNT);
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
@@ -106,25 +98,25 @@ map.classList.remove('map--faded');
 var mapPins = document.querySelector('.map__pins');
 var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
-var createPinElement = function (template, array, X, Y) {
-  var element = template.cloneNode(true);
+var createPinElement = function (template, ad, offsetX, offsetY) {
+  var pin = template.cloneNode(true);
 
-  element.style.left = array.location.x - X + 'px';
-  element.style.top = array.location.y - Y + 'px';
-  element.querySelector('img').src = array.author.avatar;
-  element.querySelector('img').alt = array.title;
+  pin.style.left = ad.location.x - offsetX + 'px';
+  pin.style.top = ad.location.y - offsetY + 'px';
+  pin.querySelector('img').src = ad.author.avatar;
+  pin.querySelector('img').alt = ad.title;
 
-  return element;
+  return pin;
 };
 
-var createDomBlock = function (count, template, array, blockLocation, X, Y) {
+var createPinsBlock = function (count, template, adsList, blockLocation, offsetX, offsetY) {
   var fragment = document.createDocumentFragment();
   for (var i = 0; i < count; i++) {
-    var blockElement = createPinElement(template, array[i], X, Y);
+    var blockElement = createPinElement(template, adsList[i], offsetX, offsetY);
 
     fragment.appendChild(blockElement);
   }
   blockLocation.appendChild(fragment);
 };
 
-createDomBlock(OBJECTS_COUNT, pinTemplate, objects, mapPins, pinX, PIN_Y);
+createPinsBlock(OBJECTS_COUNT, pinTemplate, ads, mapPins, PIN_OFFSET_X, PIN_OFFSET_Y);
